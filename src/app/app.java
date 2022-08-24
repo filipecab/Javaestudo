fe9073280922204c8a04ef76ed5a334443f35d38
Filipe Matheus <silva.filipe.filipe@gmail.com>
2022-08-24 17:26:55 -0300

    alterações na main

--- src/app/app.java
+++ src/app/app.java
@@ -16,19 +16,58 @@ public class app {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc=new Scanner(System.in);
-
-        String nome=sc.next();
-        String nivel=sc.next();
-        double salario=sc.nextDouble();
-
-        String cliente=sc.next();
-
-        LocalDate data=LocalDate.of(sc.nextInt(),sc.nextInt(),sc.nextInt());
+        funcionario func=new funcionario();
         
+
+        while(true){
+
+            System.out.println("Quantos funcionarios deseja cadastrar: ");
+            int quant=sc.nextInt();
+
+            for (int i=0;i<=quant;i++){
+                System.out.println("digite o nome do funcionario n"+i+1);
+                String nome=sc.nextLine();
+                System.out.println("qual o salario do seu funcionario?");
+                double sal=sc.nextDouble();
+                System.out.println("digite o departamento: ");
+                String dep=sc.next();
+                
+                System.out.println("Digite: 1 junior,2 Mid_level,3 senior");
+                int opc=sc.nextInt();
+               
+                if (opc==1){
+                    String nivel="JUNIOR";
+                    func=new funcionario(nome, Enumerados.JUNIOR, sal, dep);
+                }
+                else if(opc==2){
+                    String nivel="MID_LEVEL";
+                    func=new funcionario(nome, Enumerados.valueOf(nivel), sal, dep);
+
+                }
+                else if(opc==3){
+                    String nivel="SENIOR";
+                    func=new funcionario(nome, Enumerados.valueOf(nivel), sal, dep);
+                }
+                else{
+                    System.out.println("tente novamente!");
+                    i=i-1;
+                }
+                
+
+
+            }
+            
+            
+
+
+
+            sc.close();
+        }
+        
+    }
         
        
-        sc.close();
-    }
+        
 
     
 
